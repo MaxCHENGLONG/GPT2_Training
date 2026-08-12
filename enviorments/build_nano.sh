487e72a037818ba1d93a16b9669d9f403147498d
MaxCHENGLONG <asuralmax0915@outlook.com>
2026-08-12 11:21:13 +0200

    add build sh

---
+++ enviorments/build_nano.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A naiss2025-22-1730
+#SBATCH --gpus=1
+#SBATCH -t 02:00:00
+#SBATCH -J build_sif
+#SBATCH -o /nobackup/proj/disk/naiss2025-22-1730/personal/licheng/logs/%x-%j.out
+
+set -euo pipefail
+
+BASE=/nobackup/proj/disk/naiss2025-22-1730/personal/licheng
+DEF=$BASE/powermethod.def
+SIF=$BASE/powermethod.sif
+
+export APPTAINER_CACHEDIR=$BASE/apptainer_cache
+export APPTAINER_TMPDIR=$BASE/apptainer_tmp
+
+apptainer build --fakeroot --force "$SIF" "$DEF"
+
+apptainer exec --nv "$SIF" python -c "import torch; print(torch.__version__, torch.cuda.is_available())"
